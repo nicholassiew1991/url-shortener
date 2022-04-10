1bae8da4766dc6c48dd277ecf8d990a92179fe4f
nicholassiew1991 <nicholas_siew1991@hotmail.com>
2022-04-11 02:19:55 +0800

    Implement short url redirect

--- src/main/java/com/nicholas/urlshortenerapi/services/LinkService.java
+++ src/main/java/com/nicholas/urlshortenerapi/services/LinkService.java
@@ -2,7 +2,11 @@ package com.nicholas.urlshortenerapi.services;
 
 import com.nicholas.urlshortenerapi.models.Link;
 
+import java.util.Optional;
+
 public interface LinkService {
+
+  Optional<Link> getLink(String code);
 
   Link create(String originalUrl);
 }
--- src/main/java/com/nicholas/urlshortenerapi/services/implementations/LinkServiceImpl.java
+++ src/main/java/com/nicholas/urlshortenerapi/services/implementations/LinkServiceImpl.java
@@ -8,6 +8,8 @@ import com.nicholas.urlshortenerapi.services.LinkCodeGenerator;
 import com.nicholas.urlshortenerapi.services.LinkService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class LinkServiceImpl implements LinkService {
 
@@ -27,10 +29,22 @@ public class LinkServiceImpl implements LinkService {
   }
 
   @Override
+  public Optional<Link> getLink(String code) {
+    return this.linkRepository.findById(code).map(this::getLink);
+  }
+
+  @Override
   public Link create(String originalUrl) {
     String code = this.linkCodeGenerator.generateLinkCode(5);
-    String shortUrl = String.format("%s/%s", appConfigurations.getDomain(), code);
     LinkEntity entity = new LinkEntity(code, code, originalUrl);
-    return new Link(entity.getCode(), shortUrl, entity.getOriginalUrl());
+    return this.getLink(entity);
+  }
+
+  private Link getLink(LinkEntity entity) {
+    return new Link(
+      entity.getCode(),
+      String.format("%s/%s", appConfigurations.getDomain(), entity.getCode()),
+      entity.getOriginalUrl()
+    );
   }
 }
